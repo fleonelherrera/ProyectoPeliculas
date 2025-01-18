a50b2bdc4eba740bc9121cb6c8f8dd4e6523364e
franc <franc@FRANCO>
2025-01-18 19:19:46 -0300

    Agregar archivos de proyecto.

---
+++ APIRest/Model/Dto/PeliculaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace APIRest.Model.Dto
+{
+    public class PeliculaDto
+    {
+        public int IdPelicula { get; set; }
+        public string Titulo { get; set; }
+        public string Director { get; set; }
+        public string Sinopsis { get; set; }
+        public DateTime FechaEstreno { get; set; }
+    }
+}
